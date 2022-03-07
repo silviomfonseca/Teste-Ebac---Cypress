1f59cd5636962aca73b49a50c0452fd8280f27b3
silvio <silviomfonseca9@gmail.com>
2022-03-07 12:54:39 -0300

    Aula organizada e atualizada

--- cypress/integration/produtos.spec.js
+++ cypress/integration/produtos.spec.js
@@ -1,36 +1,53 @@
 /// <reference types="cypress" />
 
-describe('Funcionalidade Página de produtos', () => {
+describe('Funcionalidade Página de Produtos', () => {
+
 
     beforeEach(() => {
         cy.visit('produtos')
     });
 
-
-    it('Deve selecionar um produto da lista', () => {
+    it('Deve selecionar um produto da Lista', () => {
         cy.get('[class="product-block grid"]')
-
-            //.first()
-            //.click()
-            .contains('Ariel Roll Sleeve Sweatshirt')
+            //.first() //seleciona o primeiro item da lista
+            //.last() //seleciona o último item da lista
+            .eq(3) // seleciona o 4 item da lista
+            //.contains('Ariel Roll Sleeve Sweatshirt') // Seleciona um item da lista com esse nome
             .click()
-    })
-
-    it('Deve adicionar um produto ao carrinho ', () => {
-        var quantidade = 10
-
-        cy.get('[class="product-block grid"]')
-            .contains('Ariel Roll Sleeve Sweatshirt').click()
-            cy.get('.button-variable-item-M').click()
-            cy.get('.button-variable-item-Green').click()
-            cy.get('.input-text').clear().type(quantidade)
-            cy.get('.single_add_to_cart_button').click()
-
-            cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
-            cy.get('.woocommerce-message').should('contain',  quantidade  + ' × “Ariel Roll Sleeve Sweatshirt” foram adicionados no seu carrinho.')
     });
 
-    it.only('Deve adicionar produtos ao carrinho - Usando Comando customizado', () =>{
-        cy.addProdutos('Ariel Roll Sleeve Sweatshirt', 3)
-    })
-})
+    it('Deve Adicionar um produto ao carrinho', () => {
+        var quantidade = 3 // essa variável vai guardar o valor 3
+        cy.get('[class="product-block grid"]')
+            .eq(3).click() // Seleciona um item da lista com esse nome
+        cy.get('.button-variable-item-M').click()
+        cy.get('.button-variable-item-Green').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Ajax Full-Zip Sweatshirt” foram adicionados no seu carrinho.') // sinal de + vai concatenar a variável com a mensagem
+
+    });
+
+    //Maneira mais simples de executar um teste
+    // vai rodar usando o comands vai adicionar o produto com QTD 1, todos esse paramentros foram passados no arquivo commands
+    it('Deve adicionar produtos ao carrinho - Usando o comando customizado', () => {
+        cy.addProdutos('Argus All-Weather Tank', 'M', 'Gray', 1)
+
+    });
+
+    it('Deve adicionar produtos ao carrinho - Usando o comando customizado', () => {
+        cy.addProdutos('Aether Gym Pant', '36', 'Blue', 2)
+
+
+    });
+
+    it('Deve adicionar produtos ao carrinho - Usando o comando customizado', () => {
+        cy.addProdutos('Ajax Full-Zip Sweatshirt', 'XS', 'Green', 4)
+
+
+    });
+    // aqui você pode validar o teste com vários cenarios
+
+});
